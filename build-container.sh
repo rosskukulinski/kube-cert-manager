146fb2e16a8b44b9d6272a8c3a1681f30a03999c
Ross Kukulinski <ross@kukulinski.com>
2017-03-23 17:30:58 -0400

    Update to build my images

--- build-container.sh
+++ build-container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build -t rosskukulinski/kube-cert-manager:0.5.0 -t rosskukulinski/kube-cert-manager:latest $(dirname "$0")
-docker push rosskukulinski/kube-cert-manager:0.5.0
+docker build -t rosskukulinski/kube-cert-manager:0.5.0-san -t rosskukulinski/kube-cert-manager:latest $(dirname "$0")
+docker push rosskukulinski/kube-cert-manager:0.5.0-san
 docker push rosskukulinski/kube-cert-manager:latest
